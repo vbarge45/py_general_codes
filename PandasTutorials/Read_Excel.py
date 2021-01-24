877b9eb93132fb14d24ddfcec54b14ca4e5baf2c
vbarge45 <vbarge45@gmail.com>
2021-01-23 22:49:43 -0800

    Commit_01

---
+++ PandasTutorials/Read_Excel.py
@@ -0,0 +1,6 @@
+import pandas as pd
+df_excel = pd.read_excel('C:/Users/DESK01/Desktop/Excel Class/Dummy Data.xlsx', 'Data')
+# print(df_excel)
+# print(df_excel['Region'].unique())
+# print(pd.unique(df_excel[['Region','Country']].values.ravel('K')))
+print(df_excel.drop_duplicates(['Region','Country'])[['Region','Country']])
